ca44a5d1eb00665f2388a6389146159385fe5138
tainaoual <Tai Naoual>
2020-10-29 03:14:26 -0400

    chamge 2

--- src/main/java/com/survivingcodingbootcamp/blog/storage/repository/HashtagRepository.java
+++ src/main/java/com/survivingcodingbootcamp/blog/storage/repository/HashtagRepository.java
@@ -1,7 +1,2 @@
-package com.survivingcodingbootcamp.blog.storage.repository;
-
-import com.survivingcodingbootcamp.blog.model.HashTag;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HashTagRepository extends CrudRepository<HashTag, Long> {
+package com.survivingcodingbootcamp.blog.storage.repository;public class HashtagRepository {
 }
